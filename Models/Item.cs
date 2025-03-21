d6257758ea052f0d13a7ba08216e5f2bd77cbcd0
Sofiia Yanytska <syanytska@linfield.edu>
2025-03-21 10:05:36 -0700

    Model Item

---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace moontest1.Models
+{
+    public class Item
+    {
+        public long ItemId{ get; set; }
+        public long UserId { get; set; }
+        public long CategoryId { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int IsActive { get; set; }
+
+
+    }
+}
