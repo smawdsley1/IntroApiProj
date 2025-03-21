f92e9a7d68c7897f346e953174eb60b700e31fdf
smawdsley1 <129780236+smawdsley1@users.noreply.github.com>
2025-03-20 20:41:09 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using moontest1.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//comment
 // Build COnnection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
