404033bed113ef40de6dc5cc93d4a5db6ec020e8
Druso Escalante <descalante@linfield.edu>
2025-03-21 10:37:42 -0700

    - Created delete,put,post methods for User

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using moontest1.Data;
+using moontest1.Models;
 
 namespace moontest1.Controllers
 {
@@ -20,6 +21,34 @@ namespace moontest1.Controllers
         {
             var users = await _context.User.ToListAsync();
             return Ok(users);
+        }
+
+        [HttpPost("PostUser")]
+        public void PostUser(User user)
+        {
+            AppDbContext context = new AppDbContext();
+
+            context.User.Add(user);
+            context.SaveChanges();
+        }
+
+        [HttpPut("PutUser")]
+        public void PutUser(User user)
+        {
+            
+            
+            User userToChange = _context.User.FirstOrDefault(x => user.UserId == user.UserId);
+            userToChange.Username = user.Username;
+            _context.SaveChanges();
+            
+        }
+
+        [HttpDelete("DeleteUser/{id}")]
+        public void DeleteUser(long id)
+        {
+        
+           _context.User.Where(x => x.UserId == id).ExecuteDelete();
+            _context.SaveChanges();
         }
     }
 }
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,10 @@ namespace moontest1.Data
 {
     public class AppDbContext :DbContext
     {
+        public AppDbContext()
+        {
+        }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
         {
