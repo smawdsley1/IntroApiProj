afdb9b430fe267966c8234d5ad6f16235525dbfc
Jacob Muck <jmuck@linfield.edu>
2025-03-31 20:40:26 -0700

    .

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,9 +9,6 @@ namespace moontest1.Data
         : base(options)
         {
         }
-
-        //public DbSet<User> Users { get; set; }
-       // public DbSet<Item> Items { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<User> User { get; set; }
     }
